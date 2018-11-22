4929f98abb61e6439595fc66acd13934ca0b7f8c
Maja Djukic <maja_djkc@yahoo.com>
2018-11-22 16:43:38 +0100

    Created upload image in Add component form.

--- src/app/components/add/add.component.ts
+++ src/app/components/add/add.component.ts
@@ -6,9 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./add.component.css']
 })
 export class AddComponent {
+  imageUrl: string;
+  fileToUpload: File = null;
 
   addPin(data) {
     console.log(data);
   }
 
+  handleFileInput(file: FileList) {
+    this.fileToUpload = file.item(0);
+    const reader = new FileReader();
+    reader.onload = (event: any) => {
+      this.imageUrl = event.target.result;
+    }
+    reader.readAsDataURL(this.fileToUpload);
+  }
 }
